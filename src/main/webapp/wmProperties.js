9c6b315b11b7e7f4bacde0a5785bc3242a975107
SaraswathiRehala <saraswathi.rekhala+automation@wavemaker.com>
2017-11-22 11:12:20 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_atrUxGhPXZ",
+  "homePage" : "Main",
+  "name" : "Auto_atrUxGhPXZ",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
