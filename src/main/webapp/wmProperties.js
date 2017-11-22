ef106a32c3ed94d52f06674d5f27e87732df449d
SaraswathiRehala <saraswathi.rekhala+automation@wavemaker.com>
2017-11-22 11:14:54 +0000

    Auto commit Files modified - [securityService,profiles,webapp,project-security.xml]

--- src/main/webapp/wmProperties.js
+++ src/main/webapp/wmProperties.js
@@ -6,7 +6,7 @@ var _WM_APP_PROPERTIES = {
   "homePage" : "Main",
   "name" : "Auto_atrUxGhPXZ",
   "platformType" : "WEB",
-  "securityEnabled" : "false",
+  "securityEnabled" : "true",
   "supportedLanguages" : "en",
   "timeFormat" : "",
   "type" : "APPLICATION",
